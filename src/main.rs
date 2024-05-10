c1af549d657d5c9c2142079925d3f23243c0ae64
Arnold Smithson <arnoldsmithson@outlook.com>
2024-05-10 13:41:20 -0700

    Restructure project

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #[macro_use] extern crate rocket;
 
-use rocket::tokio::time::{sleep, Duration};
 use rocket::fs::{relative, FileServer, NamedFile, TempFile};
 use rocket::form::Form;
 use uuid::Uuid;
@@ -11,64 +10,41 @@ struct Upload<'f> {
 }
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[get("/api")]
 async fn serve_home_page() -> Result<NamedFile, std::io::Error> {
-    NamedFile::open(relative!("/www/static/index.html")).await
-}
-
-#[get("/delay/<seconds>")]
-async fn delay(seconds: u64) -> String {
-    sleep(Duration::from_secs(seconds)).await;
-    format!("Waited for {} seconds", seconds)
-}
-
-#[post("/clicked")]
-fn clicked() -> String {
-    format!("Clicked button")
-}
-
-#[get("/replaced")]
-fn replace() -> String {
-    format!("Replaced!")
+    NamedFile::open(relative!("/www/static/html/index.html")).await
 }
 
 #[post("/upload", format = "multipart/form-data", data = "<form>")]
-async fn upload(mut form: Form<Upload<'_>>) -> std::io::Result<()> {
+async fn upload(mut form: Form<Upload<'_>>) -> std::io::Result<String> {
 
     println!("form.file = {:?}", form.file);
 
-    let f_2 = String::from(if form.file.content_type().unwrap().is_gif() {".gif"} else {
-        if form.file.content_type().unwrap().is_jpeg() {".jpeg"} else {
-            if form.file.content_type().unwrap().is_png() {".png"} else {
-                if form.file.content_type().unwrap().is_svg() {".svg"} else {
-                    if form.file.content_type().unwrap().is_mp4() {".mp4"} else {
-                        if form.file.content_type().unwrap().is_mov() {".mov"} else {
-                            if form.file.content_type().unwrap().is_webm() {".webm"} else {
-                                if form.file.content_type().unwrap().is_webp() {".webp"} else {"invalid"}
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    });
+    let f_2 = match form.file.content_type().unwrap() {
+        x if x.is_gif() => ".gif",
+        x if x.is_jpeg() => ".jpeg",
+        x if x.is_png() => ".png",
+        x if x.is_svg() => ".svg",
+        x if x.is_mp4() => ".mp4",
+        x if x.is_mov() => ".mov",
+        x if x.is_webm() => ".webm",
+        x if x.is_webp() => ".webp",
+        _ => "invalid",
+    };
 
     let file_id: String = Uuid::new_v4().hyphenated().encode_lower(&mut Uuid::encode_buffer()).to_owned();
-    let file_name = String::from(relative!("/www/static/")) + &file_id + &f_2;
+    let file_name = String::from(relative!("/www/static/")) + &file_id + f_2;
 
     println!("destination = {}", file_name);
     println!("length = {} bytes", form.file.len());
 
     form.file.persist_to(file_name).await?;
 
-    Ok(())
+    let image_url = format!("/public/{}", file_id + f_2);
+
+    Ok(format!(r#"<img id="image" src="{0}" hx-get="{0}">"#, image_url))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, delay, serve_home_page, clicked, replace, upload]).mount("/public", FileServer::from(relative!("/www/static")))
+    rocket::build().mount("/", routes![serve_home_page, upload]).mount("/public", FileServer::from(relative!("/www/static")))
 }
